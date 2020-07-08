e469dd6243f317b745919043aaa9b6893498c83c
kirill <cmirnov239kirill@gmail.com>
2020-07-08 07:42:34 +0200

    store images locally

--- Frontend/src/pages/MainPage.js
+++ Frontend/src/pages/MainPage.js
@@ -49,33 +49,18 @@ function Item(props) {
 }
 
 export const MainPage = () => {
-
-
     const classes = useStyles();
-    const images = [
-        "https://cdn1.sph.harvard.edu/wp-content/uploads/sites/21/2018/07/fruitveg.jpeg",
-        "https://www.heart.org/-/media/aha/h4gm/article-images/fruit-and-vegetables.jpg?la=en&hash=65387BD7AEEF9C37F30838E6A897796400EE80F4",
-        "https://azernews.az/media/pictures/o-GROCERY-SHOPPING-TIP-VEGETABLE-VEGETABLES-HEALTHY-facebook.jpg"
-    ]
-
-    var items = [
-        {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
-            name: "Random Name #2",
-            description: "Hello World!"
-        }
-    ]
-    console.log(images)
+    function importAll(r) {
+        return r.keys().map(r);
+    }
+    var listOfImages = importAll(require.context('../static/', false, /\.(png|jpe?g|svg)$/));
     return (
         <Grid className={classes.root} container>
             <Grid item xs={2}/>
             <Grid item xs={8}>
                 <Carousel>
                     {
-                        images.map((src) => <Item props={src}/>)
+                        listOfImages.map((src) => <Item props={src}/>)
                     }
                 </Carousel>
             </Grid>
